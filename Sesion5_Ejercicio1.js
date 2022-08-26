eea522a28a9e9bafc8fac3a196e3f6c66a4f485e
tomasdeambrosi <97841539+tomasdeambrosi@users.noreply.github.com>
2022-08-26 15:08:23 -0300

    Add files via upload

---
+++ Sesion5_Ejercicio1.js
@@ -0,0 +1,11 @@
+let alturaCentimetros = 182;
+let alturaMetros = 1.82;
+
+let pesoKilos = 85.43;
+
+let alturaRedondeada = Math.ceil(alturaMetros);
+
+let pesoRedondeado = Math.floor(pesoKilos)
+
+let comparacion = Number.MAX_VALUE + 1 === Number.MAX_VALUE;
+
